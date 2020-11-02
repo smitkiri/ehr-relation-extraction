810cae51a2fefee6bee5855fb2081cd78d19778b
anujanand6 <anujanand6@gmail.com>
2020-11-01 19:22:23 -0500

    Added function to read ADE corpus

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
-from typing import List, Tuple, Callable
+from typing import List, Tuple, Callable, Dict
+from annotations import Entity, Relation
 
 import os
 import sys
@@ -6,6 +7,7 @@ from pickle import dump, load
 from IPython.core.display import display, HTML
 from ehr import HealthRecord
 import random
+import json
 
 TPL_HTML = '<span style = "background-color: {color}; border-radius: 5px;">&nbsp;{content}&nbsp;</span>'
 
@@ -64,7 +66,8 @@ def display_ehr(text, entities):
     display(HTML(render_text))
 
 
-def read_data(data_dir: str = 'data/', train_ratio: int = 0.8, 
+def read_data(data_dir: str = 'data/',
+              train_ratio: int = 0.8, 
               tokenizer: Callable[[str], List[str]] = None, 
               verbose: int = 0) -> Tuple[List[HealthRecord], List[HealthRecord]]:
     '''
@@ -113,7 +116,7 @@ def read_data(data_dir: str = 'data/', train_ratio: int = 0.8,
                               tokenizer = tokenizer)
         train_data.append(record)
         if verbose == 1:
-            drawProgressBar(idx + 1, split_idx)
+            drawProgressBar(idx + 1, split_idx)      
     
     if verbose == 1:
         print('\n\nTest Data:')
@@ -126,21 +129,135 @@ def read_data(data_dir: str = 'data/', train_ratio: int = 0.8,
         test_data.append(record)
         if verbose == 1:
             drawProgressBar(idx + 1, len(file_ids) - split_idx)
-        
+
     return (train_data, test_data)
 
 
-def generate_input_files(ehr_records: List[HealthRecord], 
-                         filename: str, max_len:int = 510, 
+def read_ade_data(ade_data_dir: str = 'ade_data/', 
+                  train_ratio: int = 0.8, 
+                  verbose: int = 0) -> Tuple[List[Dict], List[Dict]]:
+    '''
+    Reads train and test ADE data
+
+    Parameters
+    ----------
+
+    ade_data_dir : str, optional
+        Directory where the ADE data is located. The default is 'ade_data/'.
+    
+    train_ratio : int, optional
+        Percentage split of train data. The default is 0.8.
+
+    verbose : int, optional
+        1 to print reading progress, 0 otherwise. The default is 0.
+
+    Returns
+    -------
+    Tuple[List[Dict], List[Dict]]
+        Train ADE data, Test ADE data.
+
+    '''
+
+    # Get all the IDs of ADE data
+    ade_file_ids = sorted(list(set(['.'.join(fname.split('.')[:-1])\
+                                  for fname in os.listdir(ade_data_dir)\
+                                      if not fname.startswith('.')])))
+
+    # Load ADE data
+    ade_data = []
+    for idx, fid in enumerate(ade_file_ids):
+      with open(ade_data_dir+fid +'.json') as f:
+        data = json.load(f)
+        ade_data.extend(data)
+
+    random.seed(0)
+    random.shuffle(ade_data)
+
+    ade_split_idx = int(train_ratio * len(ade_data)) 
+
+    ade_train_data = process_ade_files(ade_data[:ade_split_idx])
+    if verbose == 1:
+      print("ADE Train data:")
+      print("Done.")
+
+    ade_test_data = process_ade_files(ade_data[ade_split_idx:])
+    if verbose == 1:
+      print("\n\nADE Test data:")
+      print("Done.")
+
+
+    return (ade_train_data, ade_test_data)
+
+
+def process_ade_files(ade_data):
+  ade_records = []
+
+  for ade in ade_data:
+    entities = {}
+    relations = {}
+    relation_backlog = []
+
+    # Tokens
+    tokens = ade['tokens']
+
+    # Entities
+    E_num = 1
+    for ent in ade['entities']:
+      ent_id = 'T'+"%s"%E_num
+      ent_obj = Entity(entity_id=ent_id, 
+                      entity_type=ent['type'])
+
+      r = [ent['start'], ent['end']-1]
+      r = list(map(int, r))
+      ent_obj.set_range(r)
+
+      text = ''
+      for token_ent in ade['tokens'][ent['start']:ent['end']]:
+        text += token_ent + ' '
+      ent_obj.set_text(text)
+
+      entities[ent_id] = ent_obj
+      E_num+=1 
+
+    # Relations
+    R_num = 1
+    for relation in ade['relations']:
+      rel_id = 'R'+"%s"%R_num
+      rel_details = 'ADE-Drug'
+      entity1 = "T"+str(relation['head']+1)
+      entity2 = "T"+str(relation['tail']+1)
+
+      if entity1 in entities and entity2 in entities:
+        rel = Relation(relation_id = rel_id, 
+                      relation_type = rel_details, 
+                      arg1 = entities[entity1], 
+                      arg2 = entities[entity2])
+
+        relations[rel_id] = rel
+
+      else:
+        relation_backlog.append([rel_id, rel_details, 
+                                entity1, entity2])
+      R_num+=1
+
+    ade_records.append({"tokens":tokens, "entities": entities, "relations": relations})
+  return ade_records
+
+
+def generate_input_files(ehr_records: List[HealthRecord], filename: str,
+                         ade_records: List[Dict] = None, max_len:int = 510, 
                          sep: str = ' '):
     '''
-    Write EHR records to a file.
+    Write EHR and ADE records to a file.
 
     Parameters
     ----------
     ehr_records : List[HealthRecord]
         List of EHR records.
-    
+
+    ade_records : List[Dict]
+        List of ADE records.
+
     filename : str
         File name to write to.
     
@@ -153,7 +270,7 @@ def generate_input_files(ehr_records: List[HealthRecord],
     '''
     with open(filename, 'w') as f:
       for record in ehr_records:
-        
+
         split_idx = record.get_split_points(max_len = max_len)
         labels = record.get_labels()
         tokens = record.get_tokens()
@@ -169,8 +286,32 @@ def generate_input_files(ehr_records: List[HealthRecord],
           if i != len(split_idx)-1:
             end = split_idx[i+1]
             f.write('\n')
-  
         f.write('\n')
+
+      if ade_records is not None:
+        
+        for ade in ade_records:
+          ade_tokens = ade['tokens']
+          ade_entities = ade['entities']
+
+          ent_label_map = {'Drug': 'DRUG', 'Adverse-Effect': 'ADE'}
+          ade_labels = ['O'] * len(ade_tokens)
+
+          for ent in ade_entities.values():
+            ent_type = ent.name
+            start_idx = ent.range[0]
+            end_idx = ent.range[1]
+            
+            for idx in range(start_idx, end_idx+1):
+                if idx == start_idx:
+                    ade_labels[idx] = 'B-' + ent_label_map[ent_type]
+                else:
+                    ade_labels[idx] = 'I-' + ent_label_map[ent_type]
+
+          for (token, label) in zip(ade_tokens, ade_labels):
+            f.write('{}{}{}\n'.format(token, sep, label)) 
+          f.write('\n')
+
     print("Data successfully saved in " + filename)
 
 
